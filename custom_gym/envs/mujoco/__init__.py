509a7b0391dce52e233777e8ad0b91bb8759a4fe
Mohtasib <muhtaseb89@gmail.com>
2021-02-09 15:52:23 +0000

    Added pusher visual tf env

--- custom_gym/envs/mujoco/__init__.py
+++ custom_gym/envs/mujoco/__init__.py
@@ -7,6 +7,6 @@ from custom_gym.envs.mujoco.reacher import ReacherEnv_v2
 from custom_gym.envs.mujoco.pusher import PusherEnv
 from custom_gym.envs.mujoco.pusher import PusherEnv_v2
 
-from custom_gym.envs.mujoco.half_cheetah_pixel import HalfCheetahEnvPixel
-from custom_gym.envs.mujoco.reacher_pixel import ReacherEnvPixel
-from custom_gym.envs.mujoco.pusher_pixel import PusherEnvPixel
+# from custom_gym.envs.mujoco.half_cheetah_pixel import HalfCheetahEnvPixel
+# from custom_gym.envs.mujoco.reacher_pixel import ReacherEnvPixel
+# from custom_gym.envs.mujoco.pusher_pixel import PusherEnvPixel
